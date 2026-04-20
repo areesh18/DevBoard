a72fa626a66c618868b16b7c9a9380b521ded5b6
Areesh18 <areeshzafar396@gmail.com>
2026-04-20 09:57:55 +0530

    exercise 2- http headers and Query strings

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,11 +11,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+
 	if r.Method != http.MethodGet {
 		w.Header().Set("Allow", http.MethodGet)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("DevBoard Home"))
 }
 func logList(w http.ResponseWriter, r *http.Request) {
@@ -23,14 +26,24 @@ func logList(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	param := r.URL.Query().Get("tag")
+	if strings.TrimSpace(param) != "" {
+		w.Write([]byte("Logs tagged: " + param))
+		return
+	}
+
 	w.Write([]byte("All Dev Logs"))
+
 }
+
 func resourceList(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.Header().Set("Allow", http.MethodGet)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("All Resources"))
 }
 func main() {
